f415ecd04f756a8672d2190810efaccfc4d0ea8a
Jus <106280544+JusPLP@users.noreply.github.com>
2024-06-15 15:31:21 +0200

    switched to kde

---
+++ configuration.nix
@@ -0,0 +1,106 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.systemd-boot.consoleMode = "max";
+
+
+
+  networking.hostName = "jus";
+
+  networking.networkmanager.enable = true; 
+
+
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    keyMap = "de";
+  };
+
+
+
+  services.xserver.enable = true;
+
+  services.displayManager.sddm.wayland.enable = true;  
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;  
+
+
+  services.xserver.xkb.layout = "de";
+
+
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    okular
+    elisa
+    oxygen
+    khelpcenter
+    plasma-browser-integration
+  ];
+
+
+ 
+  hardware.pulseaudio.enable = false;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+
+
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;  
+
+
+
+  # Don't forget to set a password with ‘passwd’.
+  users.users.jus = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+	spotify
+	chromium
+	vesktop
+    ];
+  };
+
+
+
+  environment.systemPackages = with pkgs; [
+	fastfetch
+	fd
+	eza
+  	
+	# Driver
+	mesa
+  ];
+
+  fonts.packages = with pkgs; [
+	noto-fonts
+	noto-fonts-emoji
+  
+	(nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly"]; })
+  ];
+
+  nixpkgs.config = {
+	allowUnfree = true;
+  };
+
+
+
+  system.stateVersion = "24.05"; # Dont change
+
+}
+
